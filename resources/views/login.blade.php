60d06dc0eb891db915bb55dea7d71ea32437ae2f
Red-Beard7 <michael.nabangi@strathmore.edu>
2021-03-03 21:58:42 +0300

    Login Done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,16 +4,17 @@
     <div class="container">
         <div class="row vh-100 d-flex justify-content-center align-items-center">
             <div class="col-sm-4">
-                <form>
+                <form action="/login" method="POST">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        @csrf
+                        <label>Email address</label>
+                        <input type="email" class="form-control" name="email" aria-label>
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" aria-label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Sign In</button>
                 </form>
             </div>
         </div>
